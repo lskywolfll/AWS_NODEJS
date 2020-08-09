2b2f02f0795ba59e17fe5dae3f6dd91a328d3d8d
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-08-09 18:19:37 -0400

    se integran cors para poder realizar consultas

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const POET = 8080;
 const HOST = '0.0.0.0';
+
+app.use(cors({ methods: ['*'], credentials: false, origin: '*' }));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
