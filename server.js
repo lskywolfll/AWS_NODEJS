0c5a3c1f02d4d93dc532c9293980f832cba20461
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-08-09 18:20:03 -0400

    se cambia el nombre malo del puerto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const POET = 8080;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 app.use(cors({ methods: ['*'], credentials: false, origin: '*' }));
